0c6576e080f81911372d0fff68c34a64ccd59c55
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-01-28 03:43:54 +0600

    first commit

---
+++ case.js
@@ -0,0 +1,23 @@
+//trim to the string
+
+const liquid = `water`;
+const drink = ` water`;
+if (drink.trim === liquid.trim) {
+  console.log(`pani khamu`);
+} else {
+  console.log(`pani khamu na`);
+}
+
+const liquin_3 = `water`;
+const liquid_2 = ` water`;
+if (liquid_2 === liquin_3) {
+  console.log(`pani khamu`);
+} else {
+  console.log(`pani khamu na`);
+}
+//uppercase & lowercase attribute apply to the string
+
+const school = `RAJ uk Uttara Model ScHool`;
+console.log(school);
+console.log(school.toUpperCase());
+console.log(school.toLowerCase());
