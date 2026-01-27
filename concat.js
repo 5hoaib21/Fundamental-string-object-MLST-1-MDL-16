87b84fe1e7edbc00af1ba3cf70110b28cf955cff
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-01-28 04:41:55 +0600

    learning : how to reverse a string and an array

---
+++ concat.js
@@ -0,0 +1,8 @@
+const first = `Shoaib`;
+const last = `Rahman`;
+
+// const fullName = first + last;
+const fullName = first + ` ` + last;
+console.log(fullName);
+
+console.log(fullName.includes(`S`));
---
+++ reverse.js
@@ -0,0 +1,4 @@
+const sentence = `01720403094`;
+console.log(sentence);
+const reversed = sentence.split(``).reverse().join(``);
+console.log(reversed);
